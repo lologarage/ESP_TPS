4b63aa8ef2d217afa21c4a929676a0d1ae01b4b7
lologarage <lolotrgeek@gmail.com>
2021-05-06 17:51:10 -0500

    generate tps signal

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,21 @@ int MOTOR_IN_PIN = 14; // high (+) or low(-)
 int TPS_1_PIN = 25; // analog signal out
 int TPS_2_PIN = 26; // analog signal out
 
-int tps_1_state = 0; // 1 = closed, 4 = open
-int tps_2_state = 0; // 1 = closed, 4 = open
-
+// Fake Signals
+int tps_1_state = 0; // 0- 255, (closed - open) represents a voltage 0 - 3.3v
+int tps_2_state = 0; // 0- 255, (closed - open) represents a voltage 0 - 3.3v
 int motor_state = 0; // 0 off, -1 reverse, 1 foward
+int tps_signal = 0; 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void runMotor() {
   int in = digitalRead(MOTOR_IN_PIN);
   int pwm = digitalRead(MOTOR_PWM_PIN);
+
+  Serial.println("Motor " + String(in) + " , " + String(pwm));
 
   if(in == 0 && pwm == 0) {
     motor_state = 0; // stopped
@@ -28,21 +31,26 @@ void runMotor() {
 
   if(in == 1 && pwm == 0) {
     motor_state = 1; // forward
-    // TODO: count time running
+    // fake tps reading
+    if(tps_1_state < 255) {
+      tps_1_state++;
+    }
   }
   if(in == 0 && pwm == 1) {
     motor_state = -1; // reverse
-    // TODO: count time running
+    // fake tps reading
+    if(tps_1_state > 0) {
+      tps_1_state--;
+    }
   }
 }
 
 void outputTPS() {
-  // amount of time motor is "running" determines fake position
-  // 0 - 255 ( 0v - 3.2v)
-  dacWrite(TPS_1_PIN, 0);
+  dacWrite(TPS_1_PIN, tps_1_state);
+  dacWrite(TPS_2_PIN, tps_1_state);
 }
 
 void loop() {
   runMotor();
-
+  outputTPS();
 }
